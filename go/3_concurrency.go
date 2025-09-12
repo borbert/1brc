9c3ee1d852dfa7d3ce330920b7d8b63a8f615352
= <=>
2025-09-12 17:10:45 -0400

    trying to improve the efficiency of the concurrency

---
+++ go/3_concurrency.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func third_concurrency(verbose bool) {
+	start := time.Now()
+
+	lines := make(chan string, 100)               // buffered channel
+	resultChan := make(chan map[string][]float64) // channel for results
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// reader
+	go func() {
+		file, _ := os.Open("../data/measurements_1b.txt")
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			lines <- scanner.Text()
+		}
+		fmt.Println("File scanned successfully")
+		close(lines)
+	}()
+
+	// Processor
+	go func() {
+		defer wg.Done()
+		cityTemps := make(map[string][]float64)
+		for line := range lines {
+			elements := strings.Split(line, ";")
+			city := elements[0]
+			temp, _ := strconv.ParseFloat(elements[1], 64)
+			cityTemps[city] = append(cityTemps[city], temp)
+		}
+		resultChan <- cityTemps
+		fmt.Println("Data processed into map successfully")
+	}()
+
+	cityTemps := <-resultChan
+	wg.Wait()
+
+	// Process the map
+	iter := 0
+	for city, temps := range cityTemps {
+		stats := Stats{Min: temps[0], Max: temps[0], Average: 0, Sum: 0, Count: 0}
+		for _, temp := range temps {
+			if temp < stats.Min {
+				stats.Min = temp
+			}
+			if temp > stats.Max {
+				stats.Max = temp
+			}
+			stats.Count++
+			stats.Sum += temp
+			stats.Average = stats.Sum / float64(stats.Count)
+		}
+		if verbose {
+			fmt.Printf(
+				"City: %s, Min: %.2f, Max: %.2f, Average: %.2f, Count: %d\n",
+				city, stats.Min, stats.Max, stats.Average, stats.Count,
+			)
+		}
+
+		iter++
+		fmt.Printf("\rProcessing Cities:  %d", iter)
+	}
+
+	fmt.Printf("\nExecution time: %s\n", time.Since(start))
+}
